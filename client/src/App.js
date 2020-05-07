84939551fea80321bae9401460afd11f823d381f
HarryC72 <haraldcarlsten@yahoo.se>
2020-05-07 15:05:37 +0200

    Added footer auth bar

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { connect } from "react-redux";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { ToastContainer } from "react-toastify";
+import Footer from "./components/Footer";
 import "react-toastify/dist/ReactToastify.css";
 
 library.add(fab);
@@ -20,7 +21,9 @@ function App({ loadUser }) {
     <div className="App">
       <StylesProvider injectFirst>
         <Navbar />
-        <AuthBar />
+        <Footer>
+          <AuthBar />
+        </Footer>
       </StylesProvider>
       <ToastContainer autoClose={3000} hideProgressBar />
     </div>
